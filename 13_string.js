d6edd643b26bdbba3669f70b877d95bc9ccea983
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-12 01:53:02 +0530

    :)

---
+++ 13_string.js
@@ -0,0 +1,21 @@
+// let naam='kumar'
+// console.log( name.length )
+// console.log( name[0] )
+// console.log( name[1] )
+// console.log( name[2] )
+// console.log( name[3] )
+// console.log( name[4] )
+
+// for(let i=0;i<naam.length;i++){
+//     console.log( naam[i] )
+// }
+
+//template literals
+// let naam1='pranjul'
+// let naam2='milan'
+// let sentence=`${naam1} and ${naam2} is the name of same person`
+// console.log(sentence)
+
+//excape sequence
+let friuit = 'app\"le'
+console.log(friuit)
