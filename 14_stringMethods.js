e09df69fada2575b6217e93ad086e8d654ee7e9c
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-13 00:16:33 +0530

    :)

---
+++ 14_stringMethods.js
@@ -0,0 +1,17 @@
+// let student1='Pranjul'
+
+// console.log( student1 )
+// console.log( student1.length )
+// console.log( student1.toUpperCase() )
+// console.log( student1.toLowerCase() )
+
+// console.log( student1.slice(1,6) )//ranju...1 to 5 only, 6 is not included
+// console.log( student1.slice(3) )//njul...3 to till end
+
+// console.log( student1.replace('jul','Kumar') )
+
+// let student1='Kumar'
+// let student2='Pranjul'
+// let student3='milan'
+// console.log( student1.concat(student2) )
+// console.log( student1.concat(student2,student3) )
