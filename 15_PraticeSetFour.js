9240fbe8c7f58ac7805bdaa192c0943261a1e27c
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-13 01:15:12 +0530

    :)

---
+++ 15_PraticeSetFour.js
@@ -0,0 +1,19 @@
+//1
+// let student1="har\""
+// console.log(student1.length)
+
+//2
+
+//3
+// let student2='Kumar'
+// console.log( student2.toLowerCase() )
+
+//4
+// let string='Please give Rs 1000'
+// let amount=string.slice('Please give Rs'.length)
+// console.log( amount )
+
+//5
+let str='kumarPranjul'
+str[4]='R'
+console.log(str)//string cannot change bcoz it is immutable
