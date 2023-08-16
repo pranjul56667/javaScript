8975ef41f3e20c8cd8ff89e859b1ebfbae8a4e13
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-17 01:04:01 +0530

    :)

---
+++ 18_arrayMethodsExtra.js
@@ -0,0 +1,48 @@
+// let arr=[1,2,3,4,5,6,7,8,9]
+
+// delete arr[5]
+// console.log(arr)
+// console.log(arr.length)
+
+// arr.push(5)
+// console.log(arr)
+// console.log(arr.length)
+
+
+// let a='kumar'
+// let b='pranjul'
+
+// let c= b.concat(a,b)
+// console.log(c)
+
+// let num1=[2,6,11,7,1,4]
+// let num2=[7,8,9,1,5]
+
+// let num3= num1.concat(num2)
+
+// console.log(num3)
+
+// num1.sort()
+// console.log(num1)
+
+// let compare=(a,b)=>{
+//     return b-a
+// }
+// let num1=[2,6,11,7,1,4]
+
+// num1.sort(compare)
+// console.log(num1)
+
+// num1.reverse()
+// console.log(num1)
+
+// let num=[2,6,11,7,1,4]
+// // console.log(num.length)
+// // num.splice(3,1,5,6,5,8)
+// // console.log(num)
+
+// let b=num.slice(2)
+// console.log(b)
+
+// let c=num.slice(2,5)
+// console.log(c)
