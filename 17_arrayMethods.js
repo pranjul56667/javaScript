701c99a58d4965eb1bb4512bd03a36d61bd9c813
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-17 00:13:46 +0530

    :)

---
+++ 17_arrayMethods.js
@@ -0,0 +1,21 @@
+let arr=[1,2,3,4,5]
+
+// let b= arr.toString()
+// console.log(b)
+// console.log( typeof(b) )
+
+// let c=arr.join('%')
+// console.log(c)
+// console.log( typeof(c) )
+
+// arr.pop()
+// console.log(arr)
+
+// arr.push(9)
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+
+arr.unshift(78)
+console.log(arr)
