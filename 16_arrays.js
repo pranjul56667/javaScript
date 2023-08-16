09ed582441161fddeaa3d9f1fe8fb1c040e9f438
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-16 20:41:01 +0530

    :)

--- 16_arrays.js
+++ 16_arrays.js
@@ -1,5 +1,23 @@
-console.log('hhhhhh')
+// let marks=[92,82,52,65,false,'not present']
 
-let y='kumar'
+//  marks[6]=99
+//  marks[6]=199
 
-console.log(y)
+//  console.log(marks[0])
+//  console.log(marks[1])
+//  console.log(marks[2])
+//  console.log(marks[3])
+//  console.log(marks[4])
+//  console.log(marks[5])
+
+//  console.log(marks[6])// length is undefined
+
+//  console.log( 'length is', marks.length)
+
+//  console.log( typeof(marks) )
+
+let arr=[12,15,18,79,54,32,41]
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
