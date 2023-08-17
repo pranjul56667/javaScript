6bce00771dc3fa6e0ef47ca61c5073ff0eeca4aa
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-17 17:26:22 +0530

    :)

---
+++ 0_praticeQuestionJavaScript.js
@@ -0,0 +1,55 @@
+//fibnocie series
+// const number =10
+// const number = parseInt(prompt('Enter the number of terms: '));
+// let n1 = 0, n2 = 1, nextTerm;
+// for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+
+//factrial number
+// const number = 5
+// if (number < 0) {
+//     console.log('Error!');
+// }
+// else if (number === 0) {
+//     console.log(`The factorial of ${number} is 1.`);
+// }
+// else {
+//     let fact = 1;
+//     for (i = 1; i <= number; i++) {
+//         fact *= i;
+//     }
+//     console.log(`The factorial of ${number} is ${fact}.`);
+// }
+
+//
+// const cartItems = [
+//     {id: 1, qty: 3, price: 300},
+//     {id: 2, qty: 2, price: 100}
+//   ]
+//   function getTotal(carItems){
+//     let total=0;
+//     for(let i=0;i<cartItems.length;i++){
+//         total= total+carItems[i].qty*carItems[i].price
+//     }
+//     console.log(total)    
+//   }
+//   getTotal(cartItems)
+
+//
+// const test = [[3, 2], [1,5], [4, 12]] //find sum of all odd numbers
+// function oddball_sum(nums) {
+//     let sum=0
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=0;j<nums[i].length;j++){
+//             if(nums[i][j]%2==0){
+//                 sum=sum+nums[i][j]
+//             }
+//         }
+//     }
+//     console.log(sum)
+// }
+// oddball_sum(test)
---
+++ 21_PraticeSetFive.js
@@ -0,0 +1,40 @@
+//1
+// let arr=[7,9,4,5,8,5,8,5]
+// let a=prompt('enter the number')
+// a=Number.parseInt(a)
+// arr.push(a)
+// console.log(arr)
+
+//2
+// let arr=[7,9,4,5,8,5,8,5]
+// let a
+// do{
+//   a=prompt('enter the number')
+//   a=Number.parseInt(a)
+//   arr.push(a)
+// }
+// while(a!==0)
+// console.log(arr)
+
+//3
+// let arr=[1,2,30,4,50,6,7,83,670]
+// let a= arr.filter((num)=>{
+//   return num%10==0
+// })
+// console.log(a)
+
+//4
+// let arr=[1,2,30,4,50,6,7,83,670]
+
+// let a= arr.map((num)=>{
+//     return num*num
+// })
+// console.log(a)
+
+//5
+let arr=[1,2,3,4,5]
+
+let a= arr.reduce((x1,x2)=>{
+    return x1*x2
+})
+console.log(a)
