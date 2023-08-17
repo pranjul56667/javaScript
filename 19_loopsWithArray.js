33eec70cc023577de1638f520f9710c25c2d8a6e
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-17 15:22:46 +0530

    :)

--- 19_loopsWithArray.js
+++ 19_loopsWithArray.js
@@ -1,10 +1,13 @@
-let num=[2,54,84,65,14]
+let num=[2,54,84,65,14,56]
 
-for(let i=0;i<num.length;i++){
-    console.log(num[i])
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i])
+// }
+
+// num.forEach((number)=>{
+//     console.log(number*number)
+// })
+
+for( let i of num){
+    console.log(i)
 }
-
-num.forEach((number)=>{
-    console.log(number*number)
-})
-
