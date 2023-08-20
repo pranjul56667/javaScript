be1fd3af356d48841a87ac7127529d86979382dc
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-08-20 22:02:22 +0530

    :)

--- 00_praticeQuestionJavaScript.js
+++ 00_praticeQuestionJavaScript.js
@@ -143,11 +143,11 @@
 // }
 // console.log(newArr)
 
-//find the output
-// let x = {}, y = { name: "Ronny" }, z = { name: "John" };
-// x[y] = { name: "Vivek" };
-// x[z] = { name: "Akki" };
-// console.log(x[y])
+// find the output
+let x = {}, y = { name: "Ronny" }, z = { name: "John" };
+x[y] = { name: "Vivek" };
+x[z] = { name: "Akki" };
+console.log(x[y])
 
 //find the output
 // new Promise(function (resolve, reject) {
@@ -166,3 +166,6 @@
 //   }).then(function (result) {
 //     alert(result); // 4
 //   });
+
+//
+console.log('kumar pranjul')
--- 20_mapFilter&Reduce.js
+++ 20_mapFilter&Reduce.js
@@ -7,12 +7,12 @@
 // })
 // console.log(temp)
 
-// let arr=[45,23,21]
-// let a=arr.map((value,index,array)=>{
-//     console.log(value,index,array)
-//     return value+index
-// })
-// console.log(a)
+let arr=[45,23,21]
+let a=arr.map((value,index,array)=>{
+    console.log(value,index,array)
+    return value+index
+})
+console.log(a)
 
 // let arr2=[45,5,23,7,21,1,3,5]
 // let a2=arr2.filter((a)=>{
@@ -20,8 +20,8 @@
 // })
 // console.log(a2)
 
-let arr3=[1,2,3,5,2,1]
-let a3=arr3.reduce((h1,h2)=>{
-    return h1+h2
-})
-console.log(a3)
+// let arr3=[1,2,3,5,2,1]
+// let a3=arr3.reduce((h1,h2)=>{
+//     return h1+h2
+// })
+// console.log(a3)
