012b75b1702828ac1186fbbc63563c9bba993e31
Kumar Pranjul <kumarpranjulkp9236@gmail.com>
2023-09-20 14:55:03 +0530

    :)

--- 27_AlertPrompt&Confirm.js
+++ 27_AlertPrompt&Confirm.js
@@ -2,4 +2,6 @@ console.log('alert, prompt & confirm')
 console.log('alert, confirm & prompt')
 console.log('prompt,alert & confirm')
 console.log('prompt,confirm & alert')
+console.log('confirm, alert & prompt')
+
 
